89de40cd70ea3fc997366ed29561322e78defd29
Nguyen Van Luan (FTEL ISC HCM) <luannv25@fpt.com.vn>
2022-08-25 10:56:26 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,5 +3,5 @@ export REGISTRY_USER=gioi-thieu-ban-be
 export REGISTRY_PASS=fUIGzbnqGo73TH6n
 
 export PROJECT_NAME=gioi-thieu-ban-be
-export SERVICE_NAME=web-admin
+export SERVICE_NAME=api
 export REGISTRY_URI=/$PROJECT_NAME/$SERVICE_NAME-base-image
