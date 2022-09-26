2b78b8cae24d382b1a6743faa3cd621377b189bb
luna_nguyen <luna28.07.1999@gmail.com>
2022-09-27 00:30:55 +0700

    update

--- env.sh
+++ env.sh
@@ -3,5 +3,5 @@ export REGISTRY_USER=informative-portal
 export REGISTRY_PASS=vThwzsmMeI6Mu25d
 
 export PROJECT_NAME=informative-portal
-export SERVICE_NAME=api
+export SERVICE_NAME=import-cronjob
 export REGISTRY_URI=/$PROJECT_NAME/$SERVICE_NAME-base-image
